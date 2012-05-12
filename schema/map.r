58cdd8638cf5740c3cd665b1965e57dae7e98239
Thomas Levine <thomas.levine@gmail.com>
2012-05-12 13:20:26 -0400

    downloading

--- schema/map.r
+++ schema/map.r
@@ -1,22 +1,32 @@
+library(RCurl)
+my.read.csv <- function(theurl){
+  # Handle SSL
+  read.csv(textConnection(getURL(theurl)))
+}
+
 # Columns with mappings
 MAPPING_COLUMNS <- -1:-2
 
-mappings <- function(){
-  # Load the mappings spreadsheet
-  mappings <- read.csv('mappings')
-  mappings$CsvUrl <- paste(
-   'https://data.cityofchicago.org/api/views/',
-    strsplit(mappings$url, '/')[-1],
+getCsvUrl <- function(theUrl){
+  paste(
+    'https://data.cityofchicago.org/api/views/',
+    tail(strsplit(theUrl, '/')[[1]], 1),
     '/rows.csv?accessType=DOWNLOAD',
     sep = ''
   )
-  rownames(mappings) <- mappings$Category
-  mappings
 }
+
+mappings <- (function(){
+  # Load the mappings spreadsheet
+  mappings <- my.read.csv('headers.csv', as.is = T)
+  mappings$CsvUrl <- sapply(mappings$DataUrl, getCsvUrl)
+  rownames(mappings) <- mappings$CsvUrl
+  mappings
+})()
 
 remap <- function (rowname){
   # Download a particular CSV and rearrange the columns.
-  read.csv(mappings[rowname, 'CsvUrl'])[mappings[rowname, MAPPING_COLUMNS]]
+  my.read.csv(mappings[rowname, 'CsvUrl'])[mappings[rowname, MAPPING_COLUMNS]]
 }
 
 combined.and.remapped <- Reduce(function(a, b){
