9d294b387fde53be029633d33991d6feaf54f793
Thomas Levine <thomas.levine@gmail.com>
2012-05-12 13:03:22 -0400

    header remappings

--- schema/map.r
+++ schema/map.r
@@ -1,21 +1,24 @@
+# Columns with mappings
 MAPPING_COLUMNS <- -1:-2
 
-# Load the mappings spreadsheet
-mappings <- read.csv('mappings')
-mappings$CsvUrl <- paste(
- 'https://data.cityofchicago.org/api/views/',
-  strsplit(mappings$url, '/')[-1],
-  '/rows.csv?accessType=DOWNLOAD',
-  sep = ''
-)
-rownames(mappings) <- mappings$Category
+mappings <- function(){
+  # Load the mappings spreadsheet
+  mappings <- read.csv('mappings')
+  mappings$CsvUrl <- paste(
+   'https://data.cityofchicago.org/api/views/',
+    strsplit(mappings$url, '/')[-1],
+    '/rows.csv?accessType=DOWNLOAD',
+    sep = ''
+  )
+  rownames(mappings) <- mappings$Category
+  mappings
+}
 
 remap <- function (rowname){
   # Download a particular CSV and rearrange the columns.
   read.csv(mappings[rowname, 'CsvUrl'])[mappings[rowname, MAPPING_COLUMNS]]
 }
 
-
-Reduce(function(a, b){
+combined.and.remapped <- Reduce(function(a, b){
   rbind(a, remap(b))
 },rownames(mappings), subset(mappings, F))
